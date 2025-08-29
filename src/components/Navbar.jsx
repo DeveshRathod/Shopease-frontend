b2d457ee24a5ef8349996e76aecc0d78f399e21f
Devesh Rathod <108536407+DeveshRathod@users.noreply.github.com>
2025-08-29 09:15:23 +0530

    v1.0.6

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -143,7 +143,7 @@ const Navbar = () => {
         onMouseEnter={checkHealth}
         onMouseLeave={clearHealth}
       >
-        <h1 className="font-bold tracking-wide">ShopEase5</h1>
+        <h1 className="font-bold tracking-wide">ShopEase</h1>
 
         {healthStatus && (
           <span
